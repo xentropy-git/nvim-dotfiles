608e45ea3438c2d9637a270c47bf7b2ab90f30e5
xentropy <claponsie@gmail.com>
2024-08-12 22:20:30 -0400

    Added telescope

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -11,4 +11,4 @@ vim.opt.expandtab = true
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-a>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
-
+vim.g.lazyvim_picker = "telescope"
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,21 @@
+return {
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 
+        'nvim-lua/plenary.nvim',
+        'BurntSushi/ripgrep',
+    },
+    keys = {
+        {
+            "<leader>ff",
+            function()
+                require('telescope.builtin').find_files()
+            end,
+        },
+        {
+            "<leader>fg",
+            function()
+                require('telescope.builtin').live_grep()
+            end,
+        }
+    }
+}
