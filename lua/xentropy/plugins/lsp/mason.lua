99246f61ca67c863ed81b24d7d6c9956303e43e1
xentropy <claponsie@gmail.com>
2024-08-27 13:44:30 -0400

    Added LSP configuration

---
+++ lua/xentropy/plugins/lsp/mason.lua
@@ -0,0 +1,32 @@
+return {
+    "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig",
+    },
+    config = function()
+        local mason = require("mason")
+        local mason_lspconfig = require("mason-lspconfig")
+
+        mason.setup({
+            ui = {
+                icons = {
+                    package_installed = "",
+                    package_uninstalled = "",
+                    package_pending = "",
+                },
+            },
+        })
+
+        mason_lspconfig.setup({
+            ensure_installed = {
+                "tsserver",
+                "html",
+                "cssls",
+                "tailwindcss",
+                "lua_ls",
+                "pyright",
+            },
+            automatic_installation = true,
+        })
+    end
+}
